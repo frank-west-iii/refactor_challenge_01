305e9926a716ac56d69b3ff36e65d7705040fd9a
Frank West <frank.west.iii@gmail.com>
2017-07-19 20:12:03 -0700

    Add method and test

--- app/models/digest_reference.rb
+++ app/models/digest_reference.rb
@@ -1,3 +1,12 @@
 class DigestReference < ApplicationRecord
   belongs_to :user
+
+  def self.find_recipients
+    result = ActiveRecord::Base.connection.execute("select distinct user_id from digest_references")
+    users = []
+    result.each do |row|
+      users << User.find(row[0].to_i)
+    end
+    users
+  end
 end
--- test/models/digest_reference_test.rb
+++ test/models/digest_reference_test.rb
@@ -4,4 +4,18 @@ class DigestReferenceTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test ".find_recipients" do
+    5.times do |i|
+      User.create(name: "User #{i}")
+    end
+    users = User.first(4)
+
+    users.each do |user|
+      rand(1..4).times do |i|
+        DigestReference.create(user: user)
+      end
+    end
+
+    assert_equal users, DigestReference.find_recipients
+  end
 end
