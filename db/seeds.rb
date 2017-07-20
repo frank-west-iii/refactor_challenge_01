73d4bf20a3bce34ac564fbbdb2b57ade5b6c0cdb
Frank West <frank.west.iii@gmail.com>
2017-07-19 20:02:40 -0700

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+20.times do |i|
+  User.create(name: "User #{i}")
+end
+
+100.times do |i|
+  user = User.limit(1).offset(rand(1..20)).first
+  DigestReference.create(user: user)
+end
